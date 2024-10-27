9ebd688e42a19dd1fb2629e49d3e683ff725d6c5
Clay Shoaf <clayshoaf@gmail.com>
2024-10-27 18:12:16 -0400

    added files

---
+++ functions.py
@@ -0,0 +1,104 @@
+import tables as t
+import random
+
+def get_volts():
+    if random.randint(0,1):
+        zipped = []
+        for i in zip(t.wye,t.volts):
+            zipped.append(i)
+        a,b = random.choice(zipped)
+        return [a,b], True
+    else:
+        return random.choice(t.voltages), False
+
+def get_tranny(tranny):
+    #tranny.kva = round(random.randint(10,500), -1)
+    tranny.kva = random.randint(1,500)
+    tranny.phase = random.choice(t.phases)
+
+    if tranny.phase == 3:
+        tranny.prim, tranny.wye_prim = get_volts()
+        tranny.sec, tranny.wye_sec = get_volts()
+        while tranny.prim == tranny.sec:
+            tranny.sec, tranny.wye_sec = get_volts()
+    elif tranny.phase == 1:
+        tranny.prim = random.choice(t.voltages)
+        tranny.sec = random.choice(t.voltages)
+        while tranny.prim == tranny.sec:
+            tranny.sec = random.choice(t.voltages)
+
+    if tranny.wye_prim or tranny.wye_sec:
+        tranny.secondary_protection = True
+    else:
+        tranny.secondary_protection = random.choice([True,False])
+    return tranny
+
+def get_amps(volts, tranny):
+    if tranny.phase == 3:
+        amps = (tranny.kva * 1000)/(volts * 1.73)
+    else:
+        amps = (tranny.kva * 1000)/(volts)
+
+    return amps
+    
+def picker(amps, up_down):
+    if up_down == 'up':
+        for idx,i in enumerate(t.standard_ocpd):
+            if i > amps:
+                return i
+    if up_down == 'down':
+        for idx,i in enumerate(t.standard_ocpd):
+            if i > amps:
+                return t.standard_ocpd[idx-1]
+
+def choose_ocpd(amps, is_primary, tranny):
+    if is_primary:
+        if tranny.secondary_protection:
+            if amps >= 9:
+                amps = amps*t.p_and_s[0]
+                ocpd = picker(amps, 'down')
+            elif 9 > amps >= 2:
+                amps = amps*t.p_and_s[1]
+                ocpd = picker(amps, 'down')
+            else:
+                amps = amps*t.p_and_s[2]
+                ocpd = picker(amps, 'down')
+        else:
+            if amps >= 9:
+                amps = amps*t.p_only[0]
+                ocpd = picker(amps, 'up')
+                tranny.note_prim = True
+            elif 9 > amps >= 2:
+                amps = amps*t.p_only[1]
+                ocpd = picker(amps, 'down')
+            else:
+                amps = amps*t.p_only[2]
+                ocpd = picker(amps, 'down')
+    else:
+        if amps >= 9:
+            amps = amps*t.p_and_s[3]
+            ocpd = picker(amps, 'up')
+            tranny.note_sec = True
+        else:
+            amps = amps*t.p_and_s[4]
+            ocpd = picker(amps, 'down')
+            tranny.note_sec = False
+    return ocpd, tranny
+
+def get_ocpd(tranny):
+    if type(tranny.prim) == list:
+        amps_p = get_amps(tranny.prim[0], tranny)
+    else:
+        amps_p = get_amps(tranny.prim, tranny)
+    if type(tranny.sec) == list:
+        amps_s = get_amps(tranny.sec[0], tranny)
+    else:
+        amps_s = get_amps(tranny.sec, tranny)
+
+    if tranny.secondary_protection:
+        ocpd_p, tranny = choose_ocpd(amps_p, True, tranny)
+        ocpd_s, tranny = choose_ocpd(amps_s, False, tranny)
+        return tranny, amps_p, amps_s, ocpd_p, ocpd_s
+    else:
+        ocpd_p, tranny = choose_ocpd(amps_p, True, tranny)
+        return tranny, amps_p, "N/A", ocpd_p, "N/"
---
+++ test.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import functions as f
+
+class xfmr():
+    def __init__(self, phase=2, wye_prim=False, wye_sec=False, prim=0, sec=0, kva=0, secondary_protection=True, note_prim=False, note_sec=False):
+        self.phase = phase
+        self.note_prim = note_prim
+        self.note_sec = note_sec
+        self.wye_prim = wye_prim
+        self.wye_sec = wye_sec
+        self.prim = prim
+        self.sec = sec
+        self.kva = kva
+        self.secondary_protection = secondary_protection
+
+class outputs():
+    def __init__(self, t_kva='', t_phase='', t_prim='', t_sec='', t_ocpd=''):
+        self.t_kva = t_kva
+        self.t_phase = t_phase
+        self.t_prim = t_prim
+        self.t_sec = t_sec
+        self.t_ocpd = t_ocpd
+
+if 'tranny' not in st.session_state:
+    st.session_state.tranny = xfmr()
+if 'o' not in st.session_state:
+    st.session_state.o = outputs()
+
+tranny = st.session_state.tranny
+o = st.session_state.o
+
+if st.button("Generate Transformer"):
+    tranny = f.get_tranny(tranny)
+
+    o.t_kva = f"{tranny.kva} kVA"
+
+    if tranny.phase == 3:
+        o.t_phase = "Three phase"
+
+        if tranny.wye_prim:
+            o.t_prim = f"Primary: {tranny.prim[0]}Y/{tranny.prim[1]}V"
+        else:
+            o.t_prim = f"Primary: {tranny.prim}V"
+
+        if tranny.wye_sec:
+            o.t_sec = f"Secondary: {tranny.sec[0]}Y/{tranny.sec[1]}V"
+        else:
+            o.t_sec = f"Secondary: {tranny.sec}V"
+
+    elif tranny.phase == 1:
+        o.t_phase = "Single phase"
+        o.t_prim = f"Primary: {tranny.prim}V"
+        o.t_sec = f"Secondary: {tranny.sec}V"
+
+    if tranny.secondary_protection:
+        o.t_ocpd = "Primary and secondary OCPD protection"
+    else:
+        o.t_ocpd = "Primary only OCPD protection"
+
+st.write('# Transformer')
+st.text(f"{o.t_kva}\n{o.t_phase}\n{o.t_prim}\n{o.t_sec}\n{o.t_ocpd}")
+
+if st.button("Solve"):
+    tranny, amps_p, amps_s, ocpd_p, ocpd_s = f.get_ocpd(tranny)
+    st.text(f"Primary Amps: {amps_p}\nSecondary Amps: {amps_s}\nPrimary OCPD: {ocpd_p}A\nSecondary OCPD: {ocpd_s}A")
+    if tranny.note_prim:
+        st.info("Note 1 applies to primary")
+    if tranny.note_sec:
+        st.info("Note 1 applies to secondary")
+    
+
+#st.write(tranny)
